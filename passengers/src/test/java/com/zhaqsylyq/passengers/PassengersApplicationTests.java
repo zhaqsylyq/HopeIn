8bf5c9da0b37bd8978901f859f8cfad85777788e
zhaqsylyq <zhaksylyk.chalgimbayev@nu.edu.kz>
2025-02-13 15:43:54 +0500

    first commit

---
+++ passengers/src/test/java/com/zhaqsylyq/passengers/PassengersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zhaqsylyq.passengers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PassengersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
